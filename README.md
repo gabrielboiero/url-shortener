0208f18cb3ab8fe3f1e7c848ae0753a469e30a16
Gabriel Boiero <gabriel.boiero@gmail.com>
2022-05-22 11:28:16 +0200

    Authentication of users and JWT support

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
 # url-shortener
 URL Shortener in Python - Assessment task
+
+#### Disclaimer
+This is a sample app intended to solve the URL-Shortener problem commonly used in developer interviews, and should not be used in a production environment due to its limitations on security and in the generator algorithm.
+
+
+
+### Random string generator algorithm
+The process follows the idea to be as simple as possible, in both, random generation and collision avoidance. Since the expected amount of URLs to be shortened are just a few, intended for testing purpose only, it does not follow any best approach or performance rules used in real production environments.
+
+### Requirements
+* Python3
+* curl (for command line testing)
+
+### Installation
+
+
+### Execution
+```shell
+
+```
+
+#### Create users
+```shell
+
+```
+
+#### Shortening URLs
+```shell
+
+```
+
+### Running tests
+```shell
+
+```
--- main.py
+++ main.py
@@ -1,24 +1,69 @@
 import re
 import random
 import string
-
+import jwt
+import datetime
 from flask import Flask, request, jsonify, redirect
+from functools import wraps
 
 data = {}
+users = {}
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'UrlShortenerApp'
+app.config['TOKEN_EXPIRATION_TIME'] = 1800  # time span in seconds
+
+
+def verify_jwt_token(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        jwt_token = request.headers.get('jwt_token')
+        if not jwt_token:
+            return jsonify({'status': 'error', 'error': 'Missing "jwt_token" in headers'}), 400
+        try:
+            jwt.decode(jwt_token, app.config['SECRET_KEY'], algorithms=['HS256'])
+        except jwt.exceptions.InvalidTokenError:
+            return jsonify({'status': 'error', 'message': 'Invalid token or expired'}), 404
+        except Exception as e:
+            return jsonify({'status': 'error', 'message': f'Exception processing token: {e}'}), 400
+
+        return f(*args, **kwargs)
+    return decorated
 
 
 @app.route('/authenticate', methods=['POST'])
 def authenticate():
-    pass
+    if 'email' in request.form and 'password' in request.form:
+        email = request.form['email']
+        if email in users and users[email] == request.form['password']:
+            jwt_token = jwt.encode(
+                {'user': email,
+                 'exp': datetime.datetime.utcnow() + datetime.timedelta(seconds=app.config['TOKEN_EXPIRATION_TIME'])
+                 },
+                app.config['SECRET_KEY'],
+                algorithm='HS256'
+            )
+            return jsonify({'jwt_token': jwt_token})
+        else:
+            return jsonify({'status': 'error', 'message': f'Invalid email or password'}), 404
+    else:
+        return jsonify({'status': 'error', 'message': 'Missing email or password'}), 400
 
 
 @app.route('/register', methods=['POST'])
 def register():
-    pass
+    if 'email' in request.form and 'password' in request.form:
+        email = request.form['email']
+        if email not in users:
+            users[email] = request.form['password']
+            return jsonify({'status': 'success', 'message': f'User {email} created successfully'}), 200
+        else:
+            return jsonify({'status': 'error', 'message': f'User {email} already exists'}), 409
+    else:
+        return jsonify({'status': 'error', 'message': 'Missing email or password'}), 400
 
 
 @app.route('/create', methods=['POST'])
+@verify_jwt_token
 def create():
     if 'url' in request.form:
         url = request.form['url']
@@ -33,6 +78,7 @@ def create():
 
 
 @app.route('/retrieve/<url>', methods=['GET'])
+@verify_jwt_token
 def retrieve(url: str):
     if url in data:
         return jsonify({'url': data[url]}), 201
